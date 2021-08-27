a90a9ecef2bd5acad4e4c45a52b6028cc9e8ef99
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2021-08-27 02:13:01 -0500

    Generate pipes randomly

---
+++ src/entities/pipe.js
@@ -0,0 +1,22 @@
+export default class Pipe {
+  constructor(x, y, width, height, color) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  update(ctx) {
+    this.x -= 2;
+
+    this.draw(ctx);
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.closePath();
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,11 +1,23 @@
 import Player from "./entities/player.js";
+import Pipe from "./entities/pipe.js";
 
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
-let player;
-
 resizeCanvas();
+
+let player;
+console.log(canvas.height);
+const pipe = new Pipe(canvas.width / 2 - 40, 0, 80, 350, "green");
+const pipe2 = new Pipe(
+  canvas.width / 2 - 40,
+  canvas.height - 394,
+  80,
+  394,
+  "green"
+);
+
+let pipes;
 
 addEventListener("resize", () => {
   resizeCanvas();
@@ -13,6 +25,12 @@ addEventListener("resize", () => {
 
 addEventListener("click", (event) => {
   player.jump();
+});
+
+addEventListener("keydown", (event) => {
+  if (event.code == "Space") {
+    player.jump();
+  }
 });
 
 function resizeCanvas() {
@@ -27,14 +45,54 @@ function resizeCanvas() {
 
 function init() {
   player = new Player(canvas.width / 2 - 32, canvas.height / 2 - 32, 2, "red");
-  console.log(player);
+  pipes = [];
+
+  setInterval(generatePipes, 2000);
 
   animate();
+}
+
+function generatePipes() {
+  const pipesHeight = calculatePipesHeight();
+  pipes.push([
+    new Pipe(canvas.width + 200, 0, 80, pipesHeight.h1, "green"),
+    new Pipe(
+      canvas.width + 200,
+      canvas.height - pipesHeight.h2,
+      80,
+      pipesHeight.h2,
+      "green"
+    ),
+  ]);
+
+  pipes.forEach((pipesPair, idx) => {
+    if (pipesPair[0].x < -pipesPair[0].width) {
+      pipes.splice(idx, 1);
+    }
+  });
+
+  console.log(pipes.length);
+}
+
+function calculatePipesHeight() {
+  const h1 = Math.floor(Math.random() * (canvas.height - 200));
+  const h2 = canvas.height - h1 - 200;
+
+  return {
+    h1: h1,
+    h2: h2,
+  };
 }
 
 function animate() {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  pipes.forEach((pipesPair) => {
+    pipesPair.forEach((pipe) => {
+      pipe.update(ctx);
+    });
+  });
+
   player.update(ctx);
 }
 
